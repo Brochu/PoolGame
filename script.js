2139950278700ee614a87df434a4ecf9b1ef7d92
Alexandre Brochu <alexbrochu1@gmail.com>
2015-05-04 16:30:07 -0400

    formattage du code ... et debut du travail pour trouver l'angle

--- script.js
+++ script.js
@@ -52,11 +52,12 @@ function drawTable(){
 }
 
 function drawStick(){
-    // Prochaine partie
-    ctx.rotate(Math.PI / 12);
+    // Trouver l'angle que fait la souris par rapport a la balle du jouer.
+    Vector
+    ctx.rotate(Math.PI / 6);
 
     ctx.beginPath();
-    ctx.rect(0, 0, 50, 50);
+    ctx.rect(0, 0, 100, 50);
     ctx.fillStyle = '#333333'
     ctx.fill();
 
@@ -65,43 +66,43 @@ function drawStick(){
 
 function Ball(posX, posY, num)
 {
-	this.pos = new Vector(posX, posY);
-	this.num = num;
+    this.pos = new Vector(posX, posY);
+    this.num = num;
 
-	this.draw = function(ctx)
-	{
-		// Fill ball color
-		ctx.beginPath();
-		ctx.fillStyle = Ball.colors[this.num][0];
-		ctx.arc(this.pos.x, this.pos.y, Ball.RADIUS, 0, 2 * Math.PI);
-		ctx.fill();
+    this.draw = function(ctx)
+    {
+        // Fill ball color
+        ctx.beginPath();
+        ctx.fillStyle = Ball.colors[this.num][0];
+        ctx.arc(this.pos.x, this.pos.y, Ball.RADIUS, 0, 2 * Math.PI);
+        ctx.fill();
 
-		if (this.num != 0)
-		{
-			//Fill stripe
-			ctx.beginPath()
-			ctx.fillStyle = Ball.colors[this.num][1];
-			ctx.arc(this.pos.x, this.pos.y, Ball.RADIUS, Math.PI / 6, 5 * (Math.PI / 6));
-			ctx.fill();
+        if (this.num != 0)
+        {
+            //Fill stripe
+            ctx.beginPath()
+            ctx.fillStyle = Ball.colors[this.num][1];
+            ctx.arc(this.pos.x, this.pos.y, Ball.RADIUS, Math.PI / 6, 5 * (Math.PI / 6));
+            ctx.fill();
 
-			ctx.beginPath()
-			ctx.arc(this.pos.x, this.pos.y, Ball.RADIUS, 7 * Math.PI / 6, 11 * (Math.PI / 6));
-			ctx.fill();
+            ctx.beginPath()
+            ctx.arc(this.pos.x, this.pos.y, Ball.RADIUS, 7 * Math.PI / 6, 11 * (Math.PI / 6));
+            ctx.fill();
 
-			// Fill white number highlight
-			ctx.beginPath();
-			ctx.fillStyle = '#FFF';
-			ctx.arc(this.pos.x, this.pos.y, 7, 0, 2 * Math.PI);
-			ctx.fill();
-			ctx.stroke();
+            // Fill white number highlight
+            ctx.beginPath();
+            ctx.fillStyle = '#FFF';
+            ctx.arc(this.pos.x, this.pos.y, 7, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.stroke();
 
-			// Ball number
-			ctx.beginPath();
-			ctx.fillStyle = '#000';
-			ctx.textAlign = 'center';
-			ctx.fillText(this.num, this.pos.x, this.pos.y + 2.5);
-		}
-	}
+            // Ball number
+            ctx.beginPath();
+            ctx.fillStyle = '#000';
+            ctx.textAlign = 'center';
+            ctx.fillText(this.num, this.pos.x, this.pos.y + 2.5);
+        }
+    }
 }
 Ball.RADIUS = 18;
 Ball.colors = [['#FFF', '#FFF'],
@@ -123,49 +124,59 @@ Ball.colors = [['#FFF', '#FFF'],
 			  ];
 function Vector(x, y)
 {
-	// changer pour un set de fonctions qui travaillent avec array associatifs 2D
-	this.x = x;
-	this.y = y;
+    // changer pour un set de fonctions qui travaillent avec array associatifs 2D
+    this.x = x;
+    this.y = y;
 
-	this.magnitude = function()
-	{
-		return Math.sqrt((x*x) + (y*y));
-	}
+    this.magnitude = function()
+    {
+        return Math.sqrt((x*x) + (y*y));
+    }
 
-	this.add = function(v)
-	{
-		return new Vector(this.x + v.x, this.y + v.y);
-	}
+    this.magnitudesqr = function()
+    {
+        return (x*x) + (y*y);
+    }
 
-	this.debugDraw = function(ctx, posX, posY)
-	{
-		// Thanks to http://stackoverflow.com/questions/808826/draw-arrow-on-canvas-tag
-		var tox = posX + this.x;
-		var toy = posY + this.y;
+    this.add = function(v)
+    {
+        return new Vector(this.x + v.x, this.y + v.y);
+    }
 
-		var headlength = 15;
-		var headAngle = Math.PI / 6;
-		var angle = Math.atan2(toy - posY, tox - posX);
+    this.sub = function(v)
+    {
+        return new Vector(this.x - v.x, this.y - v.y);
+    }
 
-		ctx.beginPath();
+    this.debugDraw = function(ctx, posX, posY)
+    {
+        // Thanks to http://stackoverflow.com/questions/808826/draw-arrow-on-canvas-tag
+        var tox = posX + this.x;
+        var toy = posY + this.y;
 
-		ctx.moveTo(posX, posY);
-		ctx.lineTo(tox, toy)
-		ctx.lineTo(tox - headlength * Math.cos(angle - headAngle), toy - headlength * Math.sin(angle - headAngle));
-		ctx.moveTo(tox, toy);
-		ctx.lineTo(tox - headlength * Math.cos(angle + headAngle), toy - headlength * Math.sin(angle + headAngle));
+        var headlength = 15;
+        var headAngle = Math.PI / 6;
+        var angle = Math.atan2(toy - posY, tox - posX);
 
-		ctx.stroke();
-	}
+        ctx.beginPath();
+
+        ctx.moveTo(posX, posY);
+        ctx.lineTo(tox, toy)
+        ctx.lineTo(tox - headlength * Math.cos(angle - headAngle), toy - headlength * Math.sin(angle - headAngle));
+        ctx.moveTo(tox, toy);
+        ctx.lineTo(tox - headlength * Math.cos(angle + headAngle), toy - headlength * Math.sin(angle + headAngle));
+
+        ctx.stroke();
+    }
 }
 
 function testVectors(context)
 {
-	var test = new Vector(-60, -80);
-	console.log(test.magnitude());
+    var test = new Vector(-60, -80);
+    console.log(test.magnitude());
 
-	test.debugDraw(context, 250, 250);
+    test.debugDraw(context, 250, 250);
 
-	var test2 = new Vector(5, 5);
-	console.log(test.add(test2));
+    var test2 = new Vector(5, 5);
+    console.log(test.add(test2));
 }
